dca2866992d558afd6a130a3031c4eaaaeaf777a
aybek-sultanov <aybek-sultanov@inbox.ru>
2021-03-06 09:55:01 +0600

    add cancelationToken

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -5,6 +5,8 @@ using Domain;
 using MediatR;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Application.Activities
 {
@@ -22,7 +24,7 @@ namespace Application.Activities
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync();
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
