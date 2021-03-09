6bcc93cc1ccf62520d1568fd19c08da88a14c157
aybek-sultanov <aybek-sultanov@inbox.ru>
2021-03-09 11:14:53 +0600

    add request to database CRUD operations

--- client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 
@@ -26,11 +27,12 @@ export default function ActivityDashboard({ activities,
     openForm,
     closeForm, 
     createOrEdit, 
-    deleteActivity }: Props,) {
+    deleteActivity,
+    submitting }: Props,) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting} />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -41,7 +43,11 @@ export default function ActivityDashboard({ activities,
 
                     />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                    <ActivityForm 
+                        closeForm={closeForm} 
+                        activity={selectedActivity} 
+                        createOrEdit={createOrEdit}
+                        submitting={submitting} />}
             </Grid.Column>
         </Grid>
     )
