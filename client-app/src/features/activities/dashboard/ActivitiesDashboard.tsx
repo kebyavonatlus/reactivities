95a940a49f91d4f637eb07098f7b40e85dc1d066
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-03-10 00:28:22 +0600

    fix: use MobX store (refactoring)

--- client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
@@ -1,21 +1,13 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 import ActivityDetais from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
-interface Props {
-    activities: Activity[];
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
 
-
-export default observer(function ActivityDashboard({ activities, createOrEdit, deleteActivity, submitting }: Props,) {
+export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
 
     const { selectedActivity, editMode } = activityStore;
@@ -23,15 +15,13 @@ export default observer(function ActivityDashboard({ activities, createOrEdit, d
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} deleteActivity={deleteActivity} submitting={submitting} />
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
                     <ActivityDetais />}
                 {editMode &&
-                    <ActivityForm
-                        createOrEdit={createOrEdit}
-                        submitting={submitting} />}
+                    <ActivityForm />}
             </Grid.Column>
         </Grid>
     )
